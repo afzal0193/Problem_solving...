651fee6a17815230571fa0a881f6c9e7b8c15cd9
Afzal khan <108002956+afzal0193@users.noreply.github.com>
2022-07-22 16:35:54 +0600

    Delete A - Wrong Subtraction.py

--- A - Wrong Subtraction.py
+++
@@ -1,9 +0,0 @@
-n,k =map(int,input().split())
-temp = n
-for _ in range(k):
-    temp = str(temp)
-    if temp[-1] =="0":
-        temp =int(temp)//10
-    else:
-        temp = int(temp) - 1
-print(temp)
