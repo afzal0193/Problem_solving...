d324f11f726bdad0eaa5855629898ea5eda9e5ef
Afzal khan <108002956+afzal0193@users.noreply.github.com>
2022-07-22 16:37:04 +0600

    Delete A - Tram.py

--- A - Tram.py
+++
@@ -1,8 +0,0 @@
-capacity = []
-N =int(input())
-passenger = 0
-for _ in range(N):
-    a,b =map(int,input().split())
-    passenger = passenger + (b-a)
-    capacity.append(passenger)
-print(max(capacity))
